710800ed78be24d4cab0cde95d3b481d14f19c37
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-02-15 21:08:36 +0300

    Add files via upload

---
+++ examine.py
@@ -0,0 +1,41 @@
+import os 
+os.system ("clear")
+print ("_____________________________________________")
+print ("|___________________________________________|")
+print ("|||||||||||||||||||||||||||||||||||||||||||||")
+print ("|||||||||||||||||||||||||||||||||||||||||||||")
+print ("_____________________________________________")
+v7x = input ("Enter the IP address:")
+print (v7x)
+
+
+print ("1.Thank you, the IP address will be checked")
+
+
+print ("2.Scan completed successfully")
+
+
+print ("3.The phone's operating system is Android")
+
+
+print ("4.Scan completed successfully")
+
+
+print ("5.Android version 13")
+
+
+print ("6.The phone version number is SM-A100F/DS")
+
+
+print ("7.Phone serial number RRF10K10FCJHHI")
+
+
+print ("8.The device supports a single chip")
+
+
+print ("9.The IME number is 728249131847")
+
+
+print ("10.Battery size 5000")
+
+
